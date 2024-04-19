c4b67bbe3f06b04fbf47b56eaaea8631e91158ce
Con <ckritikos@gmail.com>
2024-04-19 22:47:20 +1000

    Add comments in functions.py file

--- functions.py
+++ functions.py
@@ -1,8 +1,20 @@
 def add_bid(person, amt, bid_list):
+    """
+    This function adds a new entry into the appropriate dictionary
+    :param person: the name of the bidder
+    :param amt: their bid amount
+    :param bid_list: the dictionary in which the bidder and bid will be stored in
+    :return:
+    """
     bid_list[person] = amt
 
 
 def winner(bid_list):
+    """
+    This function will determine the highest bidder and print to screen who it is and wht their winning bid was
+    :param bid_list: the dictionary that has all the bidders and their bids
+    :return:
+    """
     winner_name = ''
     winner_bid = 0
 
